7f12a0f40c91a7e89c22f9e13836441950854aef
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-23 23:19:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,17 @@ def self.count
 def self.count
   @@artists << []
 end
-def self.count
-  @@genres << []
-end
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
 
 
