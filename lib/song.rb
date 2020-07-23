878c3bcaa7832f952c647c14cfe97f74525f47cf
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-23 23:26:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,37 @@
-class Song 
-attr_accessor :name, :artist, :genre
-@@count= 0 
-@@artists = []
-@@genres = []
-def initialize(name,artist,genre)
-  @name= name
-  @artist = artist
-  @genre = genre
-  @@count +=1
-  @@artists << artist
-  @@genres << genre
-end
-def self.count
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
     @@count
   end
-def self.count
-  @@artists << []
-end
-def self.genre_count
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
+      if genre_count[genre] 
+        genre_count[genre] += 1
+      else 
         genre_count[genre] = 1
       end
     end
