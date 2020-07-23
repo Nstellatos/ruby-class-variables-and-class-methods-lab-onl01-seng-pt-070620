850fe689aabbb1952a5c0e53d26261bed965d118
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-23 23:00:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song 
 attr_accessor :name, :artist, :genre
 @@song_count= 0 
+@@count
 def initialize(name,artist,genre)
   @name= name
   @artist = artist
   @genre = genre
-  @@count = 
+  
 end
   
 
